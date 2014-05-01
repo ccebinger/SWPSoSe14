fadb137fed72b236f8f2b19e749c4547a6ce6adb
jdettlaff <j.dettlaff@fu-berlin.de>
2014-05-02 01:15:55 +0200

    Backend-Doku + ErrorMessage-Methode. Backend-Klasse dokumentiert, privater Konstruktor, ErrorMessage-Methode, um die Nachricht zu einem Backend::Status zu bekommen.

--- projekt-compiler/src/backend/backend.h
+++ projekt-compiler/src/backend/backend.h
@@ -3,14 +3,47 @@
 
 #include <iostream>
 
+/**
+ * Die Klasse stellt statische Methoden für das Übersetzen des Graphen in
+ * Target-Code zur Verfügung.
+ */
 class Backend {
 public:
+	/**
+	 * Die möglichen 'Exit Codes' des Backends.
+	 */
 	enum Status {
+		/**
+		 * Erfolgreiche Übersetzung.
+		 */
 		SUCCESS
-		// TODO other statuses...
+	// TODO other statuses...
 	};
 
-	static Backend::Status generate(std::istream& graphIn, std::ostream& codeOut);
+	/**
+	 * Übersetzt den serialisierten Graphen aus graphIn in Target-Code, der
+	 * auf codeOut geschrieben wird. Gibt je nach Ergebnis einen
+	 * Backend::Status zurück.
+	 */
+	static Backend::Status Generate(std::istream& graphIn,
+			std::ostream& codeOut);
+
+	/**
+	 * Gibt eine beschreibende Fehlernachricht zu einem Backend::Status
+	 * zurück. Ist der status Backend::Status::SUCCESS, wird ein leerer String
+	 * zurückgegeben.
+	 * Typische Nutzung dieser Methode:
+	 * if (status != Backend::Status::SUCCESS) {
+	 * 		std::cerr << Backend::ErrorMessage(status) << std::endl;
+	 * }
+	 */
+	static std::string ErrorMessage(Backend::Status status);
+
+private:
+	/**
+	 * Nur statische Methoden.
+	 */
+	Backend();
 };
 
 #endif /* BACKEND_H_ */
