851b524325c79e8f13ddb224c81548f52382652c
jdettlaff <j.dettlaff@fu-berlin.de>
2014-05-01 16:24:59 +0200

    Beginn Backend-Implementierung. main.cc: Das Backend kann jetzt mit dem Parameter -DSTANDALONE_BACKEND als Standalone-Applikation kompiliert werden. Parameter und Aufrufe dokumentiert und implementiert. Backend: Gerüst-Implementierung.

---
+++ projekt-compiler/src/backend/backend.cc
@@ -0,0 +1,8 @@
+#include "backend.h"
+
+Backend::Status Backend::generate(std::istream& graphIn, std::ostream& codeOut) {
+	(void) graphIn;
+	(void) codeOut;
+	// TODO implementieren
+	return Backend::Status::SUCCESS;
+}
