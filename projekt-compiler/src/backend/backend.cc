c60fd4f2470d3794b192989b2e737818d4d09e0a
mauriceAlthoff <maurice@althoff.me>
2014-05-08 13:32:35 +0200

    code google style guide conform gemacht

--- projekt-compiler/src/backend/backend.cc
+++ projekt-compiler/src/backend/backend.cc
@@ -1,27 +1,27 @@
-#include "backend.h"
+#include <backend/backend.h>
 
 Backend::Status Backend::Generate(std::istream& graphIn,
-		std::ostream& codeOut) {
-	Graph* graph = Graph::deserialize(graphIn);
-	Backend::Status ret = Backend::Generate(*graph, codeOut);
-	delete graph;
-	return ret;
+                                  std::ostream& codeOut) {
+  Graph* graph = Graph::deserialize(graphIn);
+  Backend::Status ret = Backend::Generate(*graph, codeOut);
+  delete graph;
+  return ret;
 }
 
 Backend::Status Backend::Generate(const Graph& graph, std::ostream& codeOut) {
-	(void) graph;
-	(void) codeOut;
-	// TODO
-	return Backend::Status::SUCCESS;
+  (void) graph;
+  (void) codeOut;
+  // TODO
+  return Backend::Status::SUCCESS;
 }
 
 std::string Backend::ErrorMessage(Backend::Status status) {
-	switch (status) {
-	case Backend::Status::SUCCESS:
-		return "";
-		break;
-		// TODO handle other cases
-	default:
-		return "";
-	}
+  switch (status) {
+    case Backend::Status::SUCCESS:
+      return "";
+      break;
+      // TODO handle other cases
+    default:
+      return "";
+  }
 }
