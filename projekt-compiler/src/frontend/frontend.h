f784e32ebb8e36408cdc5474bd2e7f5ba7a4d1b7
Zelldon <zelldon91@googlemail.com>
2014-05-12 15:20:14 +0200

    #1 adjust main method, create frontend h with include guards and removed unused CSV io class

---
+++ projekt-compiler/src/frontend/frontend.h
@@ -0,0 +1,11 @@
+#ifndef FRONTEND_H
+#define FRONTEND_H
+
+#include <iostream>
+#include "Graphs.h"
+
+
+void checkVec(std::vector<std::string> result);
+int unmarshallGraph(const std::string& file, char delimiter);
+#endif // FRONTEND_H
+
