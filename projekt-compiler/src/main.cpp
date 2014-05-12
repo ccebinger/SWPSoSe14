182212a9af218eaced80e61098c2d929c0bea799
Miro Baeten <miro.baeten@fu-berlin.de>
2014-05-12 17:13:25 +0200

    =main #1

--- projekt-compiler/src/main.cpp
+++ projekt-compiler/src/main.cpp
@@ -1,68 +1,92 @@
 #include <iostream>
 #include <string.h>
 
-// main(): frontend
-#include <frontend/frontend.h>
+#include <common/parse/parseFunctions.cpp>
 
-// main(): parseFunctions
-//#include "common/parse/parseFunctions.cpp"
 
-// main(): constant pool test
-//#include "backend/unittest/constant_pool_test.cc"
-
-// main(): backend
-//#include "backend/main.cc"
 
 using namespace std;
 
 
 int main(int argc, char *argv[]) {
 
-	bool runFrontend = false;
-	bool runBackend = false;
-	bool runParseFunctions = false;
+//	bool runFrontend = false;
+//	bool runBackend = false;
+//	bool runParseFunctions = false;
+//
+//	for(int i=0; i<argc; i++) {
+//		if(strcmp(argv[i], "-f") == 0) {
+//			runFrontend = true;
+//		}
+//		else if(strcmp(argv[i], "-b") == 0) {
+//			runBackend = true;
+//		}
+//		else if(strcmp(argv[i], "-pf") == 0) {
+//			runParseFunctions = true;
+//		}
+//
+//		cout << argv[i] << endl;
+//	}
+//
+//
+//	cout << "-------------------" << endl;
+//	cout << "-f: " << runFrontend << endl;
+//	cout << "-b: " << runBackend << endl;
+//
+//
+//	if(runFrontend) {
+//		// Alte main() aus frontend
+//		unmarshallGraph("src/frontend/test_ast.csv", ';');
+//		std::cout << std::endl << std::endl;
+//		unmarshallGraph("src/frontend/test2.csv", ';');
+//		return 0;
+//	}
+//
+//
+//
+//	if(runParseFunctions) {
+//		//
+//	}
+//
+//
+//	if(runBackend) {
+////		oldBackendMain();
+//	}
 
-	for(int i=0; i<argc; i++) {
-		if(strcmp(argv[i], "-f") == 0) {
-			runFrontend = true;
-		}
-		else if(strcmp(argv[i], "-b") == 0) {
-			runBackend = true;
-		}
-		else if(strcmp(argv[i], "-pf") == 0) {
-			runParseFunctions = true;
-		}
 
-		cout << argv[i] << endl;
-	}
+	// lex source file
+	vector<RailFunction> parsed = oldParseFunctionMain(argc, argv);
 
 
-	cout << "-------------------" << endl;
-	cout << "-f: " << runFrontend << endl;
-	cout << "-b: " << runBackend << endl;
+	// parse source
 
 
 
 
 
-	if(runFrontend) {
-		// Alte main() aus frontend
-		unmarshallGraph("src/frontend/test_ast.csv", ';');
-		std::cout << std::endl << std::endl;
-		unmarshallGraph("src/frontend/test2.csv", ';');
-		return 0;
-	}
 
 
 
-	if(runParseFunctions) {
-		//oldParseFunctionMain(argc, argv);
-	}
 
 
-	if(runBackend) {
-//		oldBackendMain();
-	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
