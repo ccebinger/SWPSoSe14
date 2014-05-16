0c37d292dd57a9487e8ce3f2e184ec0bd7283b19
Zelldon <zelldon91@googlemail.com>
2014-05-16 12:18:46 +0200

    #14 create docuentation for the adjacency_list

--- projekt-compiler/src/frontend/adjacency_list.h
+++ projekt-compiler/src/frontend/adjacency_list.h
@@ -6,29 +6,66 @@
 #include <common/ast/ast.h>
 
 /**
+ * Represents the implementation of the ASG interface as adjacency list.
+ * See documentation Graph.
  *
  * @author Christopher Zell <Zelldon91@googlemail.com>
  */
 class Adjacency_list : public Graph {
  private:
+ /**
+  * Represents a type of vector from type shared_ptr of Node.
+  * Type is named NODES.
+  */
   typedef std::vector<std::shared_ptr<Node>> NODES;
+ /**
+  * Represents a type of const std::string, named str.
+  */
   typedef const std::string str;
+ /**
+  * The name of the ASG, respectively of the Rail function.
+  */
   str graph_name;
+ /**
+  * The nodes of the ASG, respectively the commands of the corresponding Rail function.
+  */
   NODES nodes;
  public:
+ /**
+  * The ctor of the adjacency list, which gets the name of the Rail function.
+  * @param name       the name of the Rail function (ASG)
+  */
   Adjacency_list(str name);
+ /**
+  * The ctor of the adjacency list, which gets the name of the Rail function and the first node.
+  * @param name       the name of the Rail function (ASG)
+  * @param start      the start node of the Rail function
+  */
   Adjacency_list(str name, std::shared_ptr<Node> start);
 
+ /**
+  * The destructor of the adjacency list.
+  * Cleans up the nodes vector and resets the shared_ptr's.
+  */
   virtual ~Adjacency_list();
+ /**
+  * The implementation only adds the node if the node not already exists in the list.
+  * If the node exists, the node will be replaced by the new one.
+  * <br/>
+  * Inherited DOC: <br/>
+  * @copydoc Graph::addNode()
+  */
   virtual void addNode(std::shared_ptr<Node> node);
   virtual void addEdge(std::shared_ptr<Node> source, std::shared_ptr<Node> dist, bool path);
+ /**
+  * Returns the begin iterator of the nodes vector.
+  * @return     the begin iterator of the nodes vector
+  */
   virtual NODES::iterator begin();
   virtual std::shared_ptr<Node> start();
   virtual std::size_t nodeCount() const;
   virtual std::shared_ptr<Node> find(int id) const;
 	virtual std::string name() const;
- private:
-  //Command getCommand(str cmd);
 };
 
 #endif /* ADJACENCY_LIST_H */
