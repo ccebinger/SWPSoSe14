6fc04b8dbbec5b5690c3c603a92fbc99f68c9eaa
Zelldon <zelldon91@googlemail.com>
2014-05-22 23:04:55 +0200

    #14 create Documentation for:  * Graphs  * IO_Exception  * Invalid_Format_Exception  * Parse_Exception

--- projekt-compiler/src/frontend/Invalid_Format_Exception.h
+++ projekt-compiler/src/frontend/Invalid_Format_Exception.h
@@ -3,6 +3,12 @@
 
 #include <exception>
 
+/**
+ * Represents a invalid format exception, which will be thrown if
+ * the ast file is not in a valid format.
+ *
+ * @author Christopher Zell <Zelldon91@googlemail.com>
+ */
 class Invalid_Format_Exception: public std::exception
 {
   virtual const char* what() const throw()
