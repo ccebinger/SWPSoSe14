c333b11e51e064db737dfe30d7ba29d92aa157f4
mauriceAlthoff <maurice@althoff.me>
2014-05-26 23:39:00 +0200

    constant pool tests in mehrere funktionen unterteilt, damit ich und clemmens sich nicht in die quere kommen

--- projekt-compiler/src/backend/unittest/constant_pool_test.cc
+++ projekt-compiler/src/backend/unittest/constant_pool_test.cc
@@ -20,54 +20,88 @@ program; if not, see <http://www.gnu.org/licenses/>.*/
 #include <sstream>
 #include <iostream>
 
-int main(int argc, char** argv) {
+using namespace std;
+
+bool testAddClassReference() {
   ConstantPool cp;
-  std::ostringstream os;
+  return false;
+}
 
-  cp.addInt(32);
-  std::cout << "test1: " << cp.addString("main.java") << std::endl;
-  std::vector<uint8_t> list = cp.getByteArray();
-  int offset  = list.size();
-  //  cp.addInt(0x22);
-//  cp.addInt(0x20);
-//  cp.addInt(0x10);
-
-  /*
-   *  1)	Byte
-   *
-   */
+/*
+ *  1)	Byte
+ *
+ */
+bool testAddByte() {
+  ConstantPool cp;
   for (int i = 0; i< 256; i++) {
     cp.addByte(i);
   }
+  return false;
+}
 
-  /*
-   *  2)	Integer
-   *
-   */
-//  for(int i=0; i<256;i++) {
-//	  cp.addInt(i);
-//  }
-//  	cp.addInt(0x10);
+/*
+ *  2)	Integer
+ *
+ */
+bool testAddInt() {
+  ConstantPool cp;
+
+  cp.addInt(32);
+  cout << "test1: " << cp.addString("main.java") << endl;
+  auto list = cp.getByteArray();
+  int offset  = list.size();
+  //  cp.addInt(0x22);
+  //  cp.addInt(0x20);
+  //  cp.addInt(0x10);
+
+  //  for(int i=0; i<256;i++) {
+  //	  cp.addInt(i);
+  //  }
+  //  	cp.addInt(0x10);
 
   /*
    *  3)	Integer
    *
    */
+  return false;
+}
 
-  /*
-   *  4)	String
-   *
-   */
+/*
+ *  4)	String
+ *
+ */
+bool testAddString() {
+  ConstantPool cp;
+
   //cp.addString("Hallo");
 
-  std::string str;
-  list = cp.getByteArray();
+  string str;
+  auto list = cp.getByteArray();
   auto iter = list.begin();
 
   for (; iter!= list.end(); iter++) {
-    std::cout << *iter;
+    cout << *iter;
   }
-  std::cout << str << std::endl;
+  cout << str << endl;
+  return false;
+}
+
+int main(int argc, char** argv) {
+  if (!testAddClassReference()) {
+    cout << "testAddClassReference failed all system off, everything falls apart ... boom\n";
+  }
+
+  if (!testAddByte()) {
+    cout << "testAddByte failed all system off, everything falls apart ... boom\n";
+  }
+
+  if (!testAddInt()) {
+    cout << "testAddInt failed all system off, everything falls apart ... boom\n";
+  }
+
+  if (!testAddString()) {
+    cout << "testAddString failed all system off, everything falls apart ... boom\n";
+  }
 }
 
 #endif
