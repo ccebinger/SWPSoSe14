d0d84b43a2e1f4e358cc124586cca27f98655b85
kretzschi <ksascha@zedat.fu-berlin.de>
2014-05-28 09:48:46 +0200

    #38 framework with todos for MS2

--- projekt-compiler/src/backend/codegen/bytecode_generator.cpp
+++ projekt-compiler/src/backend/codegen/bytecode_generator.cpp
@@ -23,6 +23,46 @@ std::vector<char> BytecodeGenerator::GenerateCodeFromFunctionGraph(Graphs::Graph
       //
     // TODO
     break;
+    case Command::Type::ADD:
+          // Emit
+          //
+        // TODO: MS2
+        break;
+    case Command::Type::SUB:
+		  // Emit
+		  //
+		// TODO: MS2
+		break;
+    case Command::Type::MULT:
+		  // Emit
+		  //
+		// TODO: MS2
+		break;
+    case Command::Type::DIV:
+		  // Emit
+		  //
+		// TODO: MS2
+		break;
+    case Command::Type::MOD:
+		  // Emit
+		  //
+		// TODO: MS2
+		break;
+    case Command::Type::CUT:
+		  // Emit
+		  //
+		// TODO: MS2
+		break;
+    case Command::Type::APPEND:
+		  // Emit
+		  //
+		// TODO: MS2
+		break;
+    case Command::Type::SIZE:
+		  // Emit
+		  //
+		// TODO: MS2
+		break;
     default:
     // TODO errors etc.
     break;
