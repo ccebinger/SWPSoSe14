45bd8f272458673c6dd12e51fa041d8b9068eeb4
Zelldon <zelldon91@googlemail.com>
2014-06-01 22:28:43 +0200

    #63 create for each used opcode a field in BytecodeGenerator class

--- projekt-compiler/src/backend/codegen/bytecode_generator.h
+++ projekt-compiler/src/backend/codegen/bytecode_generator.h
@@ -18,6 +18,22 @@ private:
 
   typedef void (*func_ptr) (ConstantPool& pool, std::vector<char>& code, Graphs::Node_ptr current_node);
   static const std::map<Command::Type, func_ptr> CODE_FUNC_MAPPING;
+
+
+  static const char ILOAD_0 = '\x1a';
+  static const char ICONST_0 = '\x03';
+  static const char ISTORE_0 = '\x3b';
+  static const char ALOAD_1 = '\x2b';
+  static const char ASTORE_1 = '\x4c';
+  static const char INVOKE_VIRTUAL = '\xb6';
+  static const char GET_STATIC = '\xb2';
+  static const char RETURN = '\xb1';
+  static const char LDC = '\x12';
+  static const char IADD = '\x60';
+  static const char ISUB = '\x64';
+  static const char IMULT = '\x68';
+  static const char IDIV = '\x6c';
+  static const char IREM = '\x70';
 };
 
 void output_ByteCode(ConstantPool& pool, std::vector<char>& code, Graphs::Node_ptr current_node);
