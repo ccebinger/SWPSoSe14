31dbdf7b8ab6ff27ce8ee5063ce0eda6f7389947
s0taka <vincent.bohlen@fu-berlin.de>
2014-06-18 19:10:48 +0200

    added typo and missing get_static #158

--- projekt-compiler/src/backend/codegen/bytecode_generator.cpp
+++ projekt-compiler/src/backend/codegen/bytecode_generator.cpp
@@ -614,7 +614,10 @@ uint16_t get_stack_field_ref(ConstantPool& constant_pool)
 
 void globalstack_pop(ConstantPool& constant_pool, std::vector<char>& code) {
 
-  uint16_t pop_idx = BytecodeGenerator::add_method("ArrayDequeue", "pop", "()Ljava/lang/Object;", constant_pool);
+  uint16_t field_system_idx = BytecodeGenerator::add_field("Main", "stack", "Ljava/util/ArrayDeque;", constant_pool);
+  BytecodeGenerator::add_static_field(field_system_idx, constant_pool, code);
+
+  uint16_t pop_idx = BytecodeGenerator::add_method("java/lang/ArrayDeque", "pop", "()Ljava/lang/Object;", constant_pool);
   BytecodeGenerator::add_invoke_virtual(pop_idx, constant_pool, code);
 
 }
@@ -635,7 +638,7 @@ void globalstack_push(ConstantPool& constant_pool, std::vector<char>& code) {
   if (stack_field == 0)
     stack_field = get_stack_field_ref(constant_pool);
 
-  uint16_t push_idx = BytecodeGenerator::add_method("java/util/ArrayDequeue", "push", "(Ljava/lang/Object;)V", constant_pool);
+  uint16_t push_idx = BytecodeGenerator::add_method("java/util/ArrayDeque", "push", "(Ljava/lang/Object;)V", constant_pool);
    BytecodeGenerator::add_invoke_virtual(push_idx, constant_pool, code);
 }
 
