4ede2f8821e2853a18113a9d2bbb9588a9ecf357
Zelldon <zelldon91@googlemail.com>
2014-06-20 16:50:05 +0200

    #172 fix vtable bug (must impl destructor, add ctor and build method

--- projekt-compiler/src/backend/codegen/Bytecode.cpp
+++ projekt-compiler/src/backend/codegen/Bytecode.cpp
@@ -1,2 +1,11 @@
-#include "Bytecode.h"
+#include <backend/codegen/Bytecode.h>
 
+codegen::Bytecode::Bytecode(ConstantPool& p) : pool(p) {}
+
+codegen::Bytecode::~Bytecode() {}
+
+
+codegen::Bytecode* codegen::Bytecode::build(Graphs::Graph_ptr graph)
+{
+  return this;
+}
