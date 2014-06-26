c40b84756ac16ac20dc76a3e8524b3bd28675844
Miro B <miro.baeten@fu-berlin.de>
2014-06-26 21:25:29 +0200

    #208 - Lexer: removed set, changed vector to map to store rail functions properly

--- projekt-compiler/src/main.cpp
+++ projekt-compiler/src/main.cpp
@@ -40,13 +40,13 @@ int main(int argc, char *argv[]) {
 
 			// Parser
 			Env::printCaption("Frontend - Parser");
-			for(auto it = lexer.functions.begin(); it < lexer.functions.end(); ++it) {
-				Parser p(*it);
+			for(auto fun : lexer.functions) {
+				Parser p(fun.second);
 				shared_ptr<Adjacency_list> asg = p.parseGraph();
 				if(asg == NULL) {
 					throw EnvException(FRONTEND_PARSER, "No ASG to return");
 				}
-				graphs.put((*it)->getName(), asg);
+				graphs.put(fun.first, asg);
 			}
 			Env::showStatus();
 		}
